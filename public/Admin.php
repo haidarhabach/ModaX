4bd2a7a693751b9069bba055079003b3a95c48d5
Hassanj120033 <142444820+Hassanj120033@users.noreply.github.com>
2025-11-26 17:39:42 +0200

    Update Admin.php

--- public/Admin.php
+++ public/Admin.php
@@ -1,17 +1,28 @@
 <?php
 include 'db.php';
+$stmt = $connect->prepare("select id from products");
+    $stmt->execute();
+    $result=$stmt->get_result();
+    $id = mt_rand(1, 10000000);
+    while ($row = $result->fetch_assoc()) {
+        if ($row["id"] != $id) {
+            break;
+        } else {
+            $id = mt_rand(1, 10000000);
+        }
+    }
 //from add admin form m 3melneha ba3ed ha k ui 
 $name    = $_POST['name'];
 $category= $_POST['category'];
-$color   = $_POST['color'];
-$type    = $_POST['type'];
+$description   = $_POST['description'];
+$price    = $_POST['price'];
 $quantity= $_POST['quantity'];
-
+$priceSale= $_POST['priceSale'];
 $stmt = $connect->prepare("
     INSERT INTO products (name, category, color, type, quantity)
-    VALUES (?, ?, ?, ?, ?)
+    VALUES (?,?, ?,?, ?, ?, ?,now())
 ");
-$stmt->bind_param("ssssi", $name, $category, $color, $type, $quantity);
+$stmt->bind_param("iissddi",$id,$category, $name,$description, $price, $priceSale, $quantity);
 $stmt->execute();
 
 // eza lproduct lzedne sar fi meno kameye 
