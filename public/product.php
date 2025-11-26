4bec4cc1734c20b04b58a0175a67c85c639a033a
Hassanj120033 <142444820+Hassanj120033@users.noreply.github.com>
2025-11-26 17:04:02 +0200

    Update product.php

---
+++ public/product.php
@@ -0,0 +1,30 @@
+<?php
+include 'db.php';
+//demo backend 
+$id = $_GET['id'];
+$stmt = $connect->prepare("SELECT * FROM products WHERE id=?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$product = $stmt->get_result()->fetch_assoc();
+?>
+
+<h1><?= $product['name'] ?></h1>
+<p>Category: <?= $product['category'] ?></p>
+<p>Color: <?= $product['color'] ?></p>
+<p>Type: <?= $product['type'] ?></p>
+
+<?php if ($product['quantity'] == 0): ?>
+    <h3>out of stock</h3>
+
+    <form action="notify_me.php" method="POST">
+        <input type="hidden" name="product_id" value="<?= $product['id'] ?>">
+        <input type="hidden" name="category" value="<?= $product['category'] ?>">
+        <input type="hidden" name="color" value="<?= $product['color'] ?>">
+        <input type="hidden" name="type" value="<?= $product['type'] ?>">
+
+        <label>Enter your email to notify:</label>
+        <input type="email" name="email" required >
+
+        <button type="submit">Notify me</button>
+    </form>
+<?php endif; ?>
